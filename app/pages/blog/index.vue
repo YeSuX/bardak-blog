f67e6f4e0736a06be82e90095868712897dcdab4
YeSuX <351490949@qq.com>
2025-12-02 23:25:43 +0800

    Remove app.vue and add blog index page with dynamic post listing

---
+++ app/pages/blog/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <h1>我的博客</h1>
+
+        <ul>
+            <li v-for="post in posts" :key="post.slug">
+                <NuxtLink :to="`/blog/${post.slug}`">{{ post.title }}</NuxtLink>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+const { data: posts } = await useAsyncData('posts', () =>
+    queryContent('blog').find()
+)
+</script>
