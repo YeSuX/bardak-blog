6c18b9293091ad68c4605b6625bf5bee32f12db7
YeSuX <351490949@qq.com>
2025-12-03 13:13:33 +0800

    Implement dynamic page routing for blog posts and remove legacy blog components; add initial Markdown content for the first blog post.

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const route = useRoute()
+
+const { data: page } = await useAsyncData(route.path, () => {
+    return queryCollection('content').path(route.path).first()
+})
+
+console.log(page.value, route.path);
+
+</script>
+
+<template>
+    <div>
+        <header><!-- ... --></header>
+
+        <ContentRenderer v-if="page" :value="page" />
+
+        <footer><!-- ... --></footer>
+    </div>
+</template>
--- app/pages/blog/[slug].vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="container">
-        <ContentDoc />
-    </div>
-</template>
